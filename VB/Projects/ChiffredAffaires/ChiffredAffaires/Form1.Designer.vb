dfdffedf5062eedec340091de400c9e2d549606a
Maxime Liénard <lienard.maxime@gmail.com>
2015-11-21 11:24:51 +0100

    Ajout code VB sur le chiffre d'affaire
    Signed-off-by: Maxime Liénard <lienard.maxime@gmail.com>

---
+++ VB/Projects/ChiffredAffaires/ChiffredAffaires/Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmChiffre
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblCa = New System.Windows.Forms.Label()
+        Me.edtCa = New System.Windows.Forms.TextBox()
+        Me.btnQuit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblCa
+        '
+        Me.lblCa.AutoSize = True
+        Me.lblCa.Location = New System.Drawing.Point(104, 28)
+        Me.lblCa.Name = "lblCa"
+        Me.lblCa.Size = New System.Drawing.Size(39, 13)
+        Me.lblCa.TabIndex = 0
+        Me.lblCa.Text = "Label1"
+        '
+        'edtCa
+        '
+        Me.edtCa.Location = New System.Drawing.Point(107, 44)
+        Me.edtCa.Name = "edtCa"
+        Me.edtCa.Size = New System.Drawing.Size(100, 20)
+        Me.edtCa.TabIndex = 1
+        '
+        'btnQuit
+        '
+        Me.btnQuit.Location = New System.Drawing.Point(117, 88)
+        Me.btnQuit.Name = "btnQuit"
+        Me.btnQuit.Size = New System.Drawing.Size(75, 23)
+        Me.btnQuit.TabIndex = 2
+        Me.btnQuit.Text = "Button1"
+        Me.btnQuit.UseVisualStyleBackColor = True
+        '
+        'frmChiffre
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(324, 123)
+        Me.Controls.Add(Me.btnQuit)
+        Me.Controls.Add(Me.edtCa)
+        Me.Controls.Add(Me.lblCa)
+        Me.Name = "frmChiffre"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblCa As System.Windows.Forms.Label
+    Friend WithEvents edtCa As System.Windows.Forms.TextBox
+    Friend WithEvents btnQuit As System.Windows.Forms.Button
+
+End Class
---
+++ VB/Projects/ChiffredAffaires/ChiffredAffaires/Form1.vb
@@ -0,0 +1,67 @@
+﻿Public Class frmChiffre
+    Dim intro, phrase, endPh, message, mois(11), de(1) As String
+    Dim CA As Double
+
+    Private Sub frmChiffre_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        intro = "Vous n'avez que 2 possibilités:" & vbCr & "- Presser OK autorise la saisie de la suite des résultats mensuels" & vbCr & "- Presser Annuler termine immédiatement l'application"
+        phrase = "Entrez le chiffre d'affaires du mois"
+        mois = {"janvier", "février", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "décembre"}
+        endPh = ":"
+        de = {" de ", " d'"}
+
+        lblCa.Text = "Chiffre d'affaires annuel:"
+        btnQuit.Text = "Quitter"
+        edtCa.ReadOnly = True
+
+        CA = 0
+        prgmLoop()
+
+    End Sub
+
+    Private Function prgmLoop()
+        Dim reponse As Byte
+        reponse = MessageBox.Show(intro, "Aide à l'utilisateur", MessageBoxButtons.OKCancel, MessageBoxIcon.Information)
+        Select Case reponse
+            Case DialogResult.OK
+
+            Case DialogResult.Cancel
+                End
+        End Select
+        For i As Byte = 0 To 11 Step 1
+            If i = 3 Or i = 7 Or i = 9 Then
+                message = phrase & de(1) & mois(i) & endPh
+            Else
+                message = phrase & de(0) & mois(i) & endPh
+            End If
+            CA = CA + getCM(message)
+        Next
+        edtCa.Text = Format(Val(CA), "0.00" & " €")
+    End Function
+    Private Function getCM(ByVal message As String) As Double
+        Dim temp As String
+        Dim nCounter As Integer
+        Do
+            nCounter = 0
+            Do
+                temp = InputBox(message, "CA mensuel", "")
+                For i As Integer = 1 To temp.Length Step 1
+                    If Strings.Mid(temp, i, 1) = "." Then
+                        nCounter = nCounter + 1
+                    ElseIf Strings.Mid(temp, i, 1) = "," Then
+                        nCounter = nCounter + 2
+                    End If
+                Next
+
+                If nCounter > 1 Or IsNumeric(temp) = 0 Then
+                    MessageBox.Show("Entrée incorrecte !", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                End If
+            Loop Until (IsNumeric(temp) And IsDBNull(temp) = False)
+        Loop While nCounter > 1
+
+        Return Val(temp)
+    End Function
+
+    Private Sub btnQuit_Click(sender As Object, e As EventArgs) Handles btnQuit.Click
+        End
+    End Sub
+End Class
